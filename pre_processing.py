3c2ab0f544456bca65c9662895d44524f37ffc30
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-07-15 23:57:03 +0200

    Update pre_processing.py

--- pre_processing.py
+++ pre_processing.py
@@ -7,7 +7,7 @@ df = pd.read_csv('EU_EV.csv')
 
 print(df)
 print(df.describe())
-
+''' scaling
 min_year= df['year'].min()
 max_year = df['year'].max()
 df['year'] = df['year'].apply(lambda x: (x-min_year)/(max_year-min_year))
@@ -28,6 +28,11 @@ max_sales_PHEV = df['sales_PHEV'].max()
 avg_sales_PHEV = df['sales_PHEV'].mean()
 df['sales_PHEV'] = df['sales_PHEV'].apply(lambda x: (x-avg_sales_PHEV)/(max_sales_PHEV-min_sales_PHEV))
 
+min_sales_BEV = df['sales_BEV'].min()
+max_sales_BEV = df['sales_BEV'].max()
+avg_sales_BEV = df['sales_BEV'].mean()
+df['sales_BEV'] = df['sales_BEV'].apply(lambda x: (x-avg_sales_BEV)/(max_sales_BEV-min_sales_BEV))
+
 min_stock_BEV = df['stock_BEV'].min()
 max_stock_BEV = df['stock_BEV'].max()
 avg_stock_BEV = df['stock_BEV'].mean()
@@ -38,14 +43,31 @@ max_stock_PHEV = df['stock_PHEV'].max()
 avg_stock_PHEV = df['stock_PHEV'].mean()
 df['stock_PHEV'] = df['stock_PHEV'].apply(lambda x: (x-avg_stock_PHEV)/(max_stock_PHEV-min_stock_PHEV))
 
+min_EV_stock_share= df['EV_stock_share'].min()
+max_EV_stock_share = df['EV_stock_share'].max()
+avg_EV_stock_share = df['EV_stock_share'].mean()
+df['EV_stock_share'] = df['EV_stock_share'].apply(lambda x: (x-avg_EV_stock_share)/(max_EV_stock_share-min_EV_stock_share))
 
-df['EV_stock_share'] = df['EV_stock_share'].apply(lambda x: x/100)
-df['EV_sales_share'] = df['EV_sales_share'].apply(lambda x: x/100)
+min_EV_sales_share= df['EV_sales_share'].min()
+max_EV_sales_share = df['EV_sales_share'].max()
+avg_EV_sales_share = df['EV_sales_share'].mean()
+df['EV_sales_share'] = df['EV_sales_share'].apply(lambda x: (x-avg_EV_sales_share)/(max_EV_sales_share-min_EV_sales_share))'''
+
+#df['EV_stock_share'] = df['EV_stock_share'].apply(lambda x: x/100)
+#df['EV_sales_share'] = df['EV_sales_share'].apply(lambda x: x/100)
 print(df)
+
+df1 = df.sample(frac = 1)
 
 # reorder dataframe
 dataset = df[['year','region','fast_charging_point','slow_charging_point','sales_BEV','sales_PHEV','EV_sales_share','stock_BEV','stock_PHEV','EV_stock_share']]
 print(dataset)
+
+Country = ['France','Belgium','Germany','Netherland','Poland','Portugal','Spain','Switzerland','United Kingdom','Denmark','Norway','Sweden','Finland']
+Country_value = np.arange(13)
+
+df = df[df['region'].isin(Country)]
+df['region'].replace(Country, Country_value, inplace=True)
 
 X = df[['year','fast_charging_point','slow_charging_point','stock_BEV','stock_PHEV','EV_stock_share']]
 y = df['sales_BEV']
@@ -56,6 +78,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 train_set = X_train.assign(sales_BEV=y_train)
 test_set = X_test.assign(sales_BEV=y_test)
 
+
 # export dataset
-#train_set.to_csv('train_set.csv')
-#test_set.to_csv('test_set.csv')
+train_set.to_csv('train_set.csv')
+
+test_set.to_csv('test_set.csv')
